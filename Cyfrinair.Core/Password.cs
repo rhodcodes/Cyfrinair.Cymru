001c003a9d66ce4e9ee0a17bafa246d41752d617
Rhodri Lloyd-Evans <1529752+rhodcodes@users.noreply.github.com>
2025-07-04 23:05:54 +0100

    Add extra tests for Password.cs
    Added more coverage for further permutations and edge cases. Also fixed some bugs unearthed by the edge case tests.

--- Cyfrinair.Core/Password.cs
+++ Cyfrinair.Core/Password.cs
@@ -5,31 +5,36 @@ namespace Cyfrinair.Core;
 
 public class Password(PasswordOptions options)
 {
-    private readonly PasswordOptions _options = options ?? throw new ArgumentNullException(nameof(options));
     public const string BaseCharacterSet = "abcdefghjkmnpqrtuvwxyzACDEFGHIJKMNPQRTUVWXYZ";
     public const string DigitSet = "234679";
     public const string SymbolSet = "!@#$%^&*()+[]{}<>?";
     public const string AmbiguousChars = "ilLoOSsB";
     public const string AmbiguousSymbols = "-_.,|";
     public const string AmbiguousDigits = "0158";
+    private readonly PasswordOptions _options = options ?? throw new ArgumentNullException(nameof(options));
 
     public List<string> Generate(ushort quantity)
     {
-        var results = new List<string>(quantity);
-        for (var i = 1; i <= quantity; i++)
+        List<string> results = new(quantity);
+        for (int i = 1; i <= quantity; i++)
         {
             results.Add(CreatePassword());
         }
-        
+
         return results;
     }
 
     private string CreatePassword()
     {
-        var availableCharacters = BaseCharacterSet;
-        var result = new StringBuilder(_options.Length);
-        var bytes = new byte[_options.Length];
-    
+        string availableCharacters = BaseCharacterSet;
+        StringBuilder result = new(_options.Length);
+        byte[] bytes = new byte[_options.Length];
+
+        if (_options.Length < 1)
+        {
+            throw new ArgumentException("Password length must be at least 1 character.", nameof(_options.Length));
+        }
+
         if (_options.IncludeSymbols)
         {
             availableCharacters += SymbolSet;
@@ -44,18 +49,18 @@ public class Password(PasswordOptions options)
         {
             availableCharacters += AmbiguousChars;
         }
-        
+
         if (_options is { IncludeSymbols: true, IncludeAmbiguousChars: true })
         {
             availableCharacters += AmbiguousSymbols;
         }
-        
+
         if (_options is { IncludeDigits: true, IncludeAmbiguousChars: true })
         {
             availableCharacters += AmbiguousDigits;
         }
-    
-        using (var rng = RandomNumberGenerator.Create())
+
+        using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
         {
             rng.GetBytes(bytes);
         }
@@ -65,7 +70,7 @@ public class Password(PasswordOptions options)
             int index = bytes[i] % availableCharacters.Length;
             result.Append(availableCharacters[index]);
         }
-            
+
         return result.ToString();
     }
 }
